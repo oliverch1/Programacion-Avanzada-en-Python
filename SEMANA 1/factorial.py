1bf237c6eb7095b99c6c448d3832db4758836c52
oliverch1 <oliverchmath@gmail.com>
2025-04-15 10:05:42 -0500

    actualizando

--- SEMANA 1/factorial.py
+++ SEMANA 1/factorial.py
@@ -1,20 +1,6 @@
-def factores_primos(n):
-    factores = []
-    div = 2
-
-    if not isinstance(n, int) or n<2:
-        return factores
+def factorial(num):
+    if num == 0:
+        return 1
     else:
-        while n!=1:
-            if n%div==0:
-                factores.append(div)
-                n//=div
-            else:
-                div +=1
-    return factores
-
-
-num = int(input("ingrese un numero entero: "))
-lista = factores_primos(num)
-print(f"\nlos factores primos son:\n{lista}")
-
+        return num * factorial(num - 1)
+    
